5e489a2a88cfdb187b9c4d62119444db03af6821
Erik van Weelderen <erikvanweelderen@hotmail.nl>
2024-11-28 11:18:02 +0100

    Version 2
    Has updated installation guide. Also has installer script.

---
+++ install.bat
@@ -0,0 +1,3 @@
+xcopy .\LM3886.lib %LocalAppData%\LTspice\lib\sub
+xcopy .\LM3886.asy %LocalAppData%\LTspice\lib\sym
+echo Copying complete... Now try to run the example.asc... If that does not work, then manually install the files using 'install.txt'
